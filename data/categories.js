780e8cf13ac5ac33cb476868f12a67141031c844
mario <mariosergio.rs94@gmail.com>
2019-03-26 10:22:22 -0500

    heasd

---
+++ data/categories.js
@@ -0,0 +1,24 @@
+   
+  
+  
+  
+   
+
+const categoriesList = [
+  {
+  	name: 'Grammys',
+  	photo: "require('./photos/uno.png')",
+  },
+  {
+  	name: 'Festival Movie',
+  	photo: "require('./photos/dos.png')",
+  },
+  {
+  	name: 'The Oscars',
+  	photo: "require('./photos/tres.png')",
+  },
+];
+
+module.exports.categoriesList=categoriesList;
+
+
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+
+const listings = require('./data/listings')
+const categoriesList = require('./data/categories')
+
+
+
+//const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/listings', (req, res) => {
+
+   res.send(listings)
+  
+  });
+
+app.get('/categorieslist', (req, res) => {
+  
+  res.send(categoriesList)
+    
+  
+  });
+
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
